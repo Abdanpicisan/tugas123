ffa4d914751b9d3392f7fdf01682f4b19fbfa840
Abdanpicisan <32300417+Abdanpicisan@users.noreply.github.com>
2018-01-20 22:34:28 +0800

    2

--- Untitled2.cpp
+++ Untitled2.cpp
@@ -2,9 +2,23 @@
 #include <string>
 using namespace std;
 
-class sepatu{
+class sepatuVans{
 	public:
-	void merk(string brand);
-	string bahan,nama;
+	void jenis(string bentuk);
+	string bahan,lini;
+
+
+};
+
+int main(){
+	
+	sepatuVans slipon,oldskool,sk8hi;
+	
+	slipon.bahan    ="canvas ";
+	slipon.lini = " classic core ";
+	oldskool.bahan    = "leather ";
+	oldskool.lini   = " vault ";
+	sk8hi.bahan = "satin ";
+	sk8hi.lini = " syndicate ";
 
 
